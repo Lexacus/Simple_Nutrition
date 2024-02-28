be533ac9e5a34d7911e1b00cc1fab1384f534b23
Lexacus <aleserr894@gmail.com>
2024-02-28 09:43:53 +0100

    feat: tracker page and settings division

---
+++ src/pages/SettingsPage.tsx
@@ -0,0 +1,77 @@
+import { ToastContainer, toast } from "react-toastify";
+import { Button } from "../components/common/Button";
+import { Footer } from "../components/layout/Footer";
+import { Clipboard } from "@capacitor/clipboard";
+import { useFoodStore } from "../store/FoodStore";
+import { useTrackerStore } from "../store/TrackerStore";
+
+const SettingsPage = () => {
+  const { days, setDays } = useTrackerStore(({ days, setDays }) => ({
+    days,
+    setDays,
+  }));
+
+  const { foods, setFoods } = useFoodStore(({ foods, setFoods }) => ({
+    foods,
+    setFoods,
+  }));
+
+  return (
+    <>
+      <div className="flex flex-col gap-y-10 mt-10">
+        <Button
+          onClick={async () => {
+            await Clipboard.write({ string: JSON.stringify(foods) });
+            toast("Successfully copied to clipboard", {
+              hideProgressBar: true,
+              type: "success",
+            });
+          }}
+        >
+          Copy food store in clipboard
+        </Button>
+        <Button
+          onClick={async () => {
+            const jsonFoods = await Clipboard.read();
+            setFoods(JSON.parse(jsonFoods.value));
+            toast("Successfully loaded from clipboard", {
+              hideProgressBar: true,
+              type: "success",
+            });
+          }}
+        >
+          Load food store from clipboard
+        </Button>
+      </div>
+      <div className="flex flex-col gap-y-10 mt-10">
+        <Button
+          onClick={async () => {
+            await Clipboard.write({ string: JSON.stringify(days) });
+            toast("Successfully copied to clipboard", {
+              hideProgressBar: true,
+              type: "success",
+            });
+          }}
+        >
+          Copy tracked days in clipboard
+        </Button>
+        <Button
+          onClick={async () => {
+            const jsonDays = await Clipboard.read();
+            setDays(JSON.parse(jsonDays.value));
+            toast("Successfully loaded from clipboard", {
+              hideProgressBar: true,
+              type: "success",
+            });
+          }}
+        >
+          Load tracked days from clipboard
+        </Button>
+      </div>
+      <Footer />
+      <ToastContainer />
+    </>
+  );
+};
+
+export default SettingsPage;
